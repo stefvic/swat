ebac6beecbaf749f33574caf0416e10c1841eae6
eugen rabii <eugen.rabii@gmail.com>
2011-08-11 12:40:45 +0000

    Custom Repository

--- src/main/java/com/swat/mercedes/be/dao/spring/jpa/custom/impl/CarRepositoryCustomImpl.java
+++ src/main/java/com/swat/mercedes/be/dao/spring/jpa/custom/impl/CarRepositoryCustomImpl.java
@@ -4,6 +4,10 @@ import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 
 import com.swat.mercedes.be.dao.spring.jpa.custom.CarRepositoryCustomInterface;
 import com.swat.mercedes.be.entities.Car;
@@ -15,6 +19,13 @@ public class CarRepositoryCustomImpl implements CarRepositoryCustomInterface {
     
     @Override
     public List<Car> findCarsInServiceForMoreThenOneMonth() {
+	CriteriaBuilder builder = entityManager.getCriteriaBuilder();
+	CriteriaQuery<Car> query = builder.createQuery(Car.class);
+	Root<Car> root = query.from(Car.class);
+	
+	//Predicate vinCode = builder.equal(root.get();
+	
+	
 	return entityManager.createQuery("from Car car where....", Car.class).getResultList();
     }
 
